bace8d2b8586fbf1a7cb252612699b7b07321147
mpoweredo <mpow3r1337@gmail.com>
2022-12-08 06:20:41 +0100

    feat: added loading spinner while fetching

--- client/src/components/todos/TodoList.tsx
+++ client/src/components/todos/TodoList.tsx
@@ -1,20 +1,20 @@
 import { useGetTodosQuery } from '@/backend/todos/todos.api'
-import { Center, Flex, Stack } from '@chakra-ui/react'
+import { Center, Flex, Spinner, Stack } from '@chakra-ui/react'
 import React from 'react'
 import TodoItem from './TodoItem/TodoItem'
 
 const TodoList = () => {
   const { data: todosData, isLoading, isFetching } = useGetTodosQuery()
 
-  console.log(todosData)
-
   return (
     <Flex mt={5} p={0} h='auto'>
       <Center flexGrow={1} px={[3, 5]}>
         <Stack direction={'column'} w='full' maxW={'33rem'} spacing={3}>
-          {todosData?.map((todo) => (
-            <TodoItem key={todo.id} {...todo} />
-          ))}
+          {isLoading || isFetching ? (
+            <Spinner mx={'auto'} />
+          ) : (
+            todosData?.map((todo) => <TodoItem key={todo.id} {...todo} />)
+          )}
         </Stack>
       </Center>
     </Flex>
